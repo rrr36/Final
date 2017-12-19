2212d64b65e5b88748ed528fd7ef23ab9220d5e4
Riddhi R Ray <rrr36@afsconnect1.njit.edu>
2017-12-19 02:36:57 -0500

    Added add()

--- model/todos.php
+++ model/todos.php
@@ -3,11 +3,28 @@ function get_t($oID) {
     global $db;
     $query = 'SELECT * FROM todos WHERE ownerid = :oid' ORDER BY 'createddate' DESC;
     $statement = $db->prepare($query);
-    $statement->bindValue(":oid", $ownerID);
+    $statement->bindValue(":oid", $oID);
     $statement->execute();
     $todos = $statement->fetch();
     $statement->closeCursor();
     return $todos;
 }
+
+function add($e, $oID, $dd, $m) {
+    global $db;
+    date_default_timezone_set("America/New_York");
+    $dt = date("Y-m-d H:i:sa");
+    $query = 'INSERT INTO todos (owneremail, ownerid, createddate, duedate, message, isdone) VALUES (:e, :oid, :cd, :dd, :m, :isd)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(":e", $e);
+    $statement->bindValue(":oid", $oID);
+    $statement->bindValue(":cd", $dt);
+    $statement->bindValue(":dd", $dd);
+    $statement->bindValue(":m", $m);
+    $statement->bindValue(":is", 0);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
 
  ?>
