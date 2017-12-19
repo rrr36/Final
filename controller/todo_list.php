525a09ed67a5a79e7e6ae3e57fd24bb0f1fb3f4b
Riddhi R Ray <rrr36@afsconnect1.njit.edu>
2017-12-19 16:00:53 -0500

    added table for incomplete and complete tasks

--- controller/todo_list.php
+++ controller/todo_list.php
@@ -24,17 +24,58 @@
                           <td><?php echo $inctodo['createdate']; ?></td>
                           <td><?php echo $inctodo['duedate']; ?></td>
                           <td><?php echo $inctodo['message']; ?></td>
+                          
                           <td><form action="." method="post">
                                   <input type="hidden" name="action" value="edit_from">
                                   <input type="hidden" name="todoID" value="<?php echo $inctodo['id']; ?>">
                                   <input type="submit" value="Edit">
                           </form></td>
+                          
+                          <td><form action="." method="post">
+                                  <input type="hidden" name="action" value="delete">
+                                  <input type="hidden" name="todoID" value="<?php echo $inctodo['id']; ?>">
+                                  <input type="hidden" name="todoDD" value="<?php echo $inctodo['duedate']; ?>">
+                                  <input type="hidden" name="todoM" value="<?php echo $inctodo['message']; ?>">
+                                  <input type="submit" value="Delete">
+                          </form></td>
+                          
+                          <td><form action="." method="post">
+                                  <input type="hidden" name="action" value="finish">
+                                  <input type="hidden" name="todoID" value="<?php echo $inctodo['id']; ?>">
+                                  <input type="submit" value="Finish">
+                          </form></td>
+                          
                       </tr>
-            <?php endforeach; ?>
-        </table>
-        <p class="last_paragraph">
-            <a href="?action=show_add_form">Add Product</a>
-        </p>
-    </section>
-</main>
-<?php include '../view/footer.php'; ?>
+                <?php }
+              } ?>
+            </table>
+        </section>
+        <section>
+            <!-- display a table of completed tasks -->
+            <h2>Completed</h2>
+            <table>
+                <tr>
+                    <th>Date Created</th>
+                    <th>Due Date</th>
+                    <th>Message</th>
+                    <th>&nbsp;</th>
+                    <th>&nbsp;</th>
+                    <th>&nbsp;</th>
+                </tr>
+                <?php foreach ($todos as $comptodo){
+                
+                    if($inctodo['isdone'] == 0){
+                        continue;
+                    }else { ?>
+                          <tr>
+                              <td><?php echo $comptodo['createdate']; ?></td>
+                              <td><?php echo $comptodo['duedate']; ?></td>
+                              <td><?php echo $comptodo['message']; ?></td>
+                              
+                          </tr>
+                    <?php }
+                  } ?>
+            </table>
+        </section>          
+  </main>
+  <?php include '../view/footer.php'; ?>
