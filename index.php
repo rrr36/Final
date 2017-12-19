0f6b5ab370a25637a68f96e6cd1a94dd6b186492
Riddhi R Ray <rrr36@afsconnect1.njit.edu>
2017-12-19 02:01:40 -0500

    Base Files

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include 'view/header.php'; ?>
+<main>
+    <h1>Menu</h1>
+    <ul>
+        <li>
+            <a href="model/login.php">Login</a>
+        </li>
+        <li>
+            <a href="model/signup.php">Sign Up</a>
+        </li>
+    </ul>
+</main>
+<?php include 'view/footer.php'; ?>
---
+++ view/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>To-Do List</title>
+    <link rel="stylesheet" type="text/css"
+          href="/~rrr36/IT218_Final/main.css">
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>To do list </h1></header>
