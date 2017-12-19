7732bd565e21e354812b6d7ce23feed3d47df24f
Riddhi R Ray <rrr36@afsconnect1.njit.edu>
2017-12-19 14:44:19 -0500

    updated index file

--- controller/index.php
+++ controller/index.php
@@ -1,53 +1,71 @@
 <?php
-require('../model/database.php');
-require('../model/product_db.php');
-require('../model/category_db.php');
-$action = filter_input(INPUT_POST, 'action');
-if ($action == NULL) {
-    $action = filter_input(INPUT_GET, 'action');
-    if ($action == NULL) {
-        $action = 'list_products';
-    }
-}
-if ($action == 'list_products') {
-    $category_id = filter_input(INPUT_GET, 'category_id', 
-            FILTER_VALIDATE_INT);
-    if ($category_id == NULL || $category_id == FALSE) {
-        $category_id = 1;
-    }
-    $category_name = get_category_name($category_id);
-    $categories = get_categories();
-    $products = get_products_by_category($category_id);
-    include('product_list.php');
-} else if ($action == 'delete_product') {
-    $product_id = filter_input(INPUT_POST, 'product_id', 
-            FILTER_VALIDATE_INT);
-    $category_id = filter_input(INPUT_POST, 'category_id', 
-            FILTER_VALIDATE_INT);
-    if ($category_id == NULL || $category_id == FALSE ||
-            $product_id == NULL || $product_id == FALSE) {
-        $error = "Missing or incorrect product id or category id.";
+  require('../model/database.php');
+  require('../model/accounts.php');
+  require('../model/todos.php');
+  
+  $action = filter_input(INPUT_POST, 'action');
+  if ($action == NULL) {
+      $action = filter_input(INPUT_GET, 'action');
+      if ($action == NULL) {
+        $action = 'todo_list';
+      }
+  }
+  if ($action == 'todo_list') {
+    $oID = getID($email,$password);
+    $firstName = getFirstName($email,$password);
+    $lastName = getLastName($email,$password);
+    $todo = get_t($oID);
+    include('todo_list.php');
+  } 
+  
+  else if ($action == 'add_form') {
+    include('add_todo.php');    
+  } 
+  else if ($action == 'add_todo') {
+    $oID = get_t($email, $password);
+    $dd = filter_input(INPUT_POST, 'dd');
+    $m = filter_input(INPUT_POST, 'm');
+    
+    if ($dd == NULL || $dd == FALSE || $m == NULL || $m == NULL) {
+        $error = "Invalid todo data. Check all fields and try again.";
         include('../errors/error.php');
     } else { 
-        delete_product($product_id);
-        header("Location: .?category_id=$category_id");
+        add($e, $oID, $dd, $m);
+        header("Location: .?action=todo_list");
     }
-} else if ($action == 'show_add_form') {
-    $categories = get_categories();
-    include('product_add.php');    
-} else if ($action == 'add_product') {
-    $category_id = filter_input(INPUT_POST, 'category_id', 
-            FILTER_VALIDATE_INT);
-    $code = filter_input(INPUT_POST, 'code');
-    $name = filter_input(INPUT_POST, 'name');
-    $price = filter_input(INPUT_POST, 'price', FILTER_VALIDATE_FLOAT);
-    if ($category_id == NULL || $category_id == FALSE || $code == NULL || 
-            $name == NULL || $price == NULL || $price == FALSE) {
-        $error = "Invalid product data. Check all fields and try again.";
+  }  
+  else if ($action == 'edit_form'){
+    $oID = filter_input(INPUT_POST, 'todoID', FILTER_VALIDATE_INT);
+    $dd = filter_input(INPUT_POST, 'todoDD');
+    $m = filter_input(INPUT_POST, 'todoM');
+    include('edit.php');
+  }
+  
+  else if ($action == 'update'){
+    $oID = filter_input(INPUT_POST, 'todoID', FILTER_VALIDATE_INT);
+    $dd = filter_input(INPUT_POST, 'todoDD');
+    $m = filter_input(INPUT_POST, 'todoM');
+    update($iOD,$dd,$m);
+    header("Location: .?action=todo_list");
+  }
+  
+  else if ($action == 'delete') {
+    $todoID = filter_input(INPUT_POST, 'todoID', FILTER_VALIDATE_INT);
+    
+    if ($todoId == NULL || $todoId == FALSE) {
+        $error = "Missing or incorrect todo id.";
         include('../errors/error.php');
     } else { 
-        add_product($category_id, $code, $name, $price);
-        header("Location: .?category_id=$category_id");
-    }
-}    
+        delete($product_id);
+        header("Location: .?todoID=$todoID");
+      }
+  }
+  
+  else if ($action == 'finish'){
+    finish();
+    header("Location: .?action=todo_list");
+  
+  }
+  
+ 
 ?>
